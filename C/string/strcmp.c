93928a3d619ec45ff6501912f84b4370943d7140
radheshyamks <radheshyams51@gmail.com>
2024-06-15 19:48:03 +0530

    modified strcmp.c file

--- C/string/strcmp.c
+++ C/string/strcmp.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define BUFF_SIZE 100
 int main(int argc, char* argv[], char** env){
+    char str[BUFF_SIZE] = "Radheshyam Kumar Singh";
     return EXIT_SUCCESS;
 }
