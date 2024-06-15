9ba74e605d7268fd6ddb14ee9366e17adfb4c938
radheshyamks <radheshyams51@gmail.com>
2024-06-15 23:58:13 +0530

    modified lambda function

--- Cpp/Lambda_function.cpp
+++ Cpp/Lambda_function.cpp
@@ -5,10 +5,26 @@
 */
 #include <iostream>
 #include <functional>
+#include <cstdint>
+
 int main()
 {
  std::function<int(void)> fptr = []()->int{std::cout<<"This is lambda function\n"; return 1;};
  int x = fptr();
  std::cout<<"x = "<< x <<std::endl;
- return 0;
+ uint32_t z=1, y=2;
+
+ // Other way to call lambda function
+//  [=]()->int{y = ++x;return x+y;}();// it is compile time error
+auto f = [=]()->int{return z+y;};
+std::cout<<f()<<std::endl;
+
+uint32_t k=0,l=0;
+std::function<int(void)> foo = [&]()->int{l = ++k; return k+l;};
+std::cout<<foo()<<"\n";
+
+uint32_t a=0, b=0;
+std::function<uint32_t(uint32_t, uint32_t)> f1 = [=](uint32_t t, uint32_t u)->uint32_t{ return t+u+z+y;};
+f1(++a, ++b);
+return 0;
 }
