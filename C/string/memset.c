dc5679609d3eec92708dd2e595ba785cdd8336d3
radheshyamks <radheshyams51@gmail.com>
2024-06-25 22:06:46 +0530

    modified memset file

--- C/string/memset.c
+++ C/string/memset.c
@@ -6,8 +6,16 @@
 #define STR_SIZE 20
 int main(int argc, char* argv[], char** env){
     char str[STR_SIZE];
+    char str1[STR_SIZE];
+    char str2[STR_SIZE];
     memset(str,0x41,STR_SIZE* sizeof(char));
+    memset(str1, 0x42, STR_SIZE* sizeof(char));
+    memset(str2, 0x43, STR_SIZE* sizeof(char));
     str[STR_SIZE-1] = '\0';
+    str1[STR_SIZE-1] = '\0';
+    str2[STR_SIZE-1] = '\0';
     printf("%s\n", str);
+    printf("%s\n", str1);
+    printf("%s\n", str2);
     return 0;
 }
