059d6e3a74a234705bb9c1f0e23205ea4b18f43a
RadheShyam <RadheShyams51@gmail.com>
2024-07-07 17:22:05 +0530

    using do while to print sum of 1 t0 100

--- C/Enum/loop.c
+++ C/Enum/loop.c
@@ -2,14 +2,21 @@
 int main()
 {
     int i=1,sum=0;
-    for(i=1;i<=100;i++)
+    /*for(i=1;i<=100;i++)
     sum=sum+i;
-    printf("%d",sum);
+    printf("%d",sum);*/
 
-    /*while(i<=100)
+   /* while(i<=100)
+    {
     sum=sum+i;
     i++;
+    }
     printf("%d",sum);*/
+    do{
+        sum=sum+i;
+        i++;
+    }while(i<=100);
+    printf("%d",sum);
     /* Adding 1-100 using for loop*/
     
     /* Adding 1-100 using while loop*/
