30240bac70fcfc1b93941911831d326275a7e316
RadheShyam <RadheShyams51@gmail.com>
2024-07-07 16:43:51 +0530

    using for loop to print sum of 1 to 100

---
+++ C/Enum/loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,sum=0;
+    for(i=1;i<=100;i++)
+    sum=sum+i;
+    printf("%d",sum);
+
+    /*while(i<=100)
+    sum=sum+i;
+    i++;
+    printf("%d",sum);*/
+    /* Adding 1-100 using for loop*/
+    
+    /* Adding 1-100 using while loop*/
+
+    /* Adding 1-100 using do-while loop*/
+    return 0;
+}
