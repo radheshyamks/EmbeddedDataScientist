dc15a16fbd470a77b4e47ae78a2d1e5a2001179a
RadheShyam <RadheShyams51@gmail.com>
2024-07-08 23:18:30 +0530

    modified ruleOfFive.cpp file

---
+++ Cpp/ruleOfFive.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class ruleOfFive{
+    private:
+        int* p_;
+    public:
+    // I. Destructor
+    // II. Copy Constructor
+    // III. Copy Assignment Operator
+    // IV. Move Constructor
+    // V. Move Assignement Operator
+};
