c0dac1b673f717da9cef12b961608271d812e6ed
RadheShyam <RadheShyams51@gmail.com>
2024-07-08 23:28:17 +0530

    modified ruleOfFive.cpp file

--- Cpp/ruleOfFive.cpp
+++ Cpp/ruleOfFive.cpp
@@ -5,9 +5,25 @@ class ruleOfFive{
     private:
         int* p_;
     public:
+    explicit ruleOfFive(int*p=nullptr){
+        this->p_ = p;
+        std::cout<<"Constructor\n";
+    }
     // I. Destructor
+    virtual ~ruleOfFive(){
+        std::cout<<"Destructor\n";
+        if(this->p_!=nullptr){
+            delete[] p_;
+        }
+    }
     // II. Copy Constructor
+    ruleOfFive(ruleOfFive& obj){
+        std::cout<<"Copy Constructor\n";
+    }
     // III. Copy Assignment Operator
+    ruleOfFive& operator=(ruleOfFive& obj){}
     // IV. Move Constructor
+    ruleOfFive(ruleOfFive&& obj){}
     // V. Move Assignement Operator
+    ruleOfFive&& operator=(ruleOfFive&& obj){}
 };
