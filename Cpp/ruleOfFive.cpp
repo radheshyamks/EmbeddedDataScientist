95759d42faec9a3093a6dc9e6208af0443991f8e
RadheShyam <RadheShyams51@gmail.com>
2024-07-09 08:44:57 +0530

    modified ruleOfFive.cpp

--- Cpp/ruleOfFive.cpp
+++ Cpp/ruleOfFive.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#define ErrCode -1
 using namespace std;
 
 class ruleOfFive{
@@ -74,20 +76,40 @@ class ruleOfFive{
         }
         return *this;
     }
+    int GetVal(){
+        if(this->p_!=nullptr){
+            return *this->p_;
+        }else{
+            return ErrCode;
+        }
+    }
+    void UpdateVal(int x){
+        if(this->p_!=nullptr){
+            *this->p_ = x;
+        }
+    }
 };
 
 int main(int argc, char* argv[], char**env){
     // I. constructor
     ruleOfFive obj1(new int(30));
+    std::cout<<obj1.GetVal()<<"\n";
     // II. Copy Constructor
     // ruleOfFive obj2=obj1;
     ruleOfFive obj2(obj1);
+    obj2.UpdateVal(11);
+    std::cout<<obj2.GetVal()<<"\n";
     // III. Copy assignment Operator
     ruleOfFive obj3;
     obj3=obj2;
+    std::cout<<obj3.GetVal()<<"\n";
     // IV. Move Constructor
+    obj3.UpdateVal(43);
     ruleOfFive obj4(std::move(obj3));
+    std::cout<<obj4.GetVal()<<"\n";
     // V. Move assignment operator
+    obj1.UpdateVal(33);
     obj4 = std::move(obj1);
+    std::cout<<obj4.GetVal()<<"\n";
     return 0;
 }
