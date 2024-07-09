f3a84d9f38be143086f88277b20c6a11b7bca7f4
RadheShyam <RadheShyams51@gmail.com>
2024-07-09 08:09:43 +0530

    modified ruleOfFive.cpp

--- Cpp/ruleOfFive.cpp
+++ Cpp/ruleOfFive.cpp
@@ -19,11 +19,60 @@ class ruleOfFive{
     // II. Copy Constructor
     ruleOfFive(ruleOfFive& obj){
         std::cout<<"Copy Constructor\n";
+        if(obj.p_!=nullptr){
+            this->p_ = new int;
+            *this->p_ = *obj.p_;
+        }
     }
     // III. Copy Assignment Operator
-    ruleOfFive& operator=(ruleOfFive& obj){}
+    ruleOfFive& operator=(ruleOfFive& obj){
+        if(this!= &obj){
+            if(this->p_!=nullptr && obj.p_!=nullptr){
+                *this->p_ = *obj.p_;
+            }else if(this->p_==nullptr && obj.p_!=nullptr){
+                this->p_ = new int;
+                *this->p_ = *obj.p_;
+            }else{
+                ;
+            }
+        }
+        return *this;
+    }
     // IV. Move Constructor
-    ruleOfFive(ruleOfFive&& obj){}
+    ruleOfFive(ruleOfFive&& obj){
+        /*if(this!=&obj){
+            if(obj.p_!=nullptr){
+                if(this->p_!=nullptr){
+                    delete[] this->p_;
+                    this->p_ = obj.p_;
+                    obj.p_ = nullptr;
+                }else{
+                    this->p_ = obj.p_;
+                    obj.p_ = nullptr;
+                }
+            }
+        }*/
+        this->p_ = obj.p_;
+        obj.p_ = nullptr;
+    }
     // V. Move Assignement Operator
-    ruleOfFive&& operator=(ruleOfFive&& obj){}
+    ruleOfFive& operator=(ruleOfFive&& obj){
+        if(this!=&obj){
+            if(obj.p_ != nullptr){
+                if(this->p_!= nullptr){
+                    delete[] this->p_;
+                    this->p_ = obj.p_;
+                    obj.p_ = nullptr;
+                }else{
+                    this->p_ = obj.p_;
+                    obj.p_ = nullptr;
+                }
+            }
+        }
+        return *this;
+    }
 };
+
+int main(int argc, char* argv[], char**env){
+    return 0;
+}
