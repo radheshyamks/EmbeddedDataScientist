6ab8b321a94c8b683635ef1055d374196662765b
RadheShyam <RadheShyams51@gmail.com>
2024-07-22 14:47:22 +0530

    modified main.cpp

--- Cpp/DesignPattern/CreationalPatterns/Singleton/src/main.cpp
+++ Cpp/DesignPattern/CreationalPatterns/Singleton/src/main.cpp
@@ -9,5 +9,8 @@ using namespace std;
 int main(int argc, char* argv[], char** env){
     SingleTon::Singleton& instance = SingleTon::Singleton::getInstance();
     SingleTon::Singleton& pinstance = SingleTon::Singleton::getParamInstance();
+    instance.UpdateUniqueId(30);
+    uint32_t id  = instance.getUniqueId();
+    std::cout<<"id: "<<id<<"\n";
     return EXIT_SUCCESS;
 }
