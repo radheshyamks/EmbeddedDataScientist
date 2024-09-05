395c75b8bf17e0128644675f4874d54f0c7430b4
RadheShyam <RadheShyams51@gmail.com>
2024-09-05 23:33:12 +0530

    modified move file

--- Cpp/MoveSemantics/move01.cpp
+++ Cpp/MoveSemantics/move01.cpp
@@ -8,11 +8,24 @@ class MoveSemanticsClass{
     private:
         T*ptr_;
     public:
-        explicit MoveSemanticsClass(T*ptr=nullptr):ptr_(ptr){}
+        explicit MoveSemanticsClass(T*ptr=nullptr):ptr_(ptr){
+            std::cout<<"It is Constructor\n";
+        }
+        explicit MoveSemanticsClass(MoveSemanticsClass&& obj){
+            if(this != &obj){
+                // if(obj.ptr_!=nullptr){
+                //     this->ptr_ = obj.ptr_;
+                // }
+                this->ptr_ = obj.ptr_;
+                obj.ptr_ = nullptr;
+            }
+            std::cout<<"It is Move Constructor\n";
+        }
         virtual ~MoveSemanticsClass(){
             if(this->ptr_!=nullptr)delete this->ptr_;
         }
         T get_val(){
+            if(this->ptr_==nullptr) return -1;
             return *this->ptr_;
         }
         void update_val(T newVal){
@@ -31,6 +44,9 @@ std::ostream& operator<<(std::ostream& out,const MoveSemanticsClass<T>& obj){
 int main(int argc, char* argv[], char** env){
     MoveSemanticsClass<int>obj(new int(100));
     std::cout<<obj;
+    std::cout<<obj.get_val()<<"\n";
+    MoveSemanticsClass<int>obj1(std::move(obj));
+    std::cout<<obj1.get_val()<<"\n";
     std::cout<<obj.get_val()<<"\n";
     return 0;
 }
