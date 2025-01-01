260a7457114bfe1eeae8873a2c505ad65424c501
RadheShyam <RadheShyams51@gmail.com>
2025-01-01 12:09:38 +0530

    modified files

---
+++ CMake/dir001/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdint>
+#include <cmath>
+#include <cstdlib>
+
+int main(int argc, char* argv[], char** env){
+    std::cout<<"Hello World\n";
+    return EXIT_SUCCESS;
+}
